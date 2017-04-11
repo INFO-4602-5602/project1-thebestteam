5971ea6320010b0167db6ce7f3b1899b12083605
danvn <dan.nguyen7@yahoo.com>
2017-04-11 17:02:39 -0600

    force csv to load before mapping points on graph

--- js/uStates.js
+++ js/uStates.js
@@ -18,8 +18,8 @@ var LongData = [];
 var LatiData = [];
 
 
-d3.csv("data/ZayoHackathonData_Buildings.csv", function(csvDataBuilding) {
-    csvDataBuilding.map(function(d){
+d3.csv("data/ZayoHackathonData_Buildings.csv", function(error, data) {
+    data.map(function(d){
         if (d.State == "CO"){
         LongData.push(+d.Longitude);
         LatiData.push(+d.Latitude);
@@ -27,19 +27,21 @@ d3.csv("data/ZayoHackathonData_Buildings.csv", function(csvDataBuilding) {
     })
     console.log("Long: ", LongData);
     console.log("Lati: ", LatiData);
-});
+
+////////////////////////////
+
 
 var map = L.map('map').setView([39.74739, -105], 13);
 
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.light'
-	}).addTo(map);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.light'
+    }).addTo(map);
 
-	var geojsonFeature = {
+    var geojsonFeature = {
     "type": "Feature",
     "properties": {
         "name": "Coors Field",
@@ -52,3 +54,34 @@ var map = L.map('map').setView([39.74739, -105], 13);
     }
 };
     L.geoJSON(geojsonFeature).addTo(map);
+
+    var zayo = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [LongData[2], LatiData[2]]
+    }
+};
+    L.geoJSON(zayo).addTo(map);    
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
