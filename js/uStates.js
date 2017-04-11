4ce138cab9472f77feb14816ec21650d7646fbb7
tamo1123 <taylormoede@gmail.com>
2017-04-11 15:03:00 -0600

    Created Long and Lati Arrays

--- js/uStates.js
+++ js/uStates.js
@@ -1,3 +1,41 @@
+//Reading in the data
+var data;
+d3.csv( 'data/ZayoHackathonData_Buildings.csv', function( csvData ){
+  data = csvData;
+  checkDataset( data );
+});
+d3.csv( 'data/ZayoHackathonData_Accounts.csv', function( csvData ){
+  data = csvData;
+  checkDataset( data );
+});
+function checkDataset(dataset) {
+    if (dataset.length > 0)
+        $("#dataCheck").append("<p>Data loaded correctly!</p>");
+    else
+        $("#dataCheck").append("<p>Data loaded incorrectly. Try using the debugger to help you find the bug!</p>");
+}
+var LongData = [];
+var LatiData = [];
+
+
+d3.csv("data/ZayoHackathonData_Buildings.csv", function(csvDataBuilding) {
+    csvDataBuilding.map(function(d){
+        LongData.push(+d.Longitude);
+        LatiData.push(+d.Latitude);
+    })
+    console.log("Long: ", LongData);
+    console.log("Lati: ", LatiData);
+    
+   /* data.forEach(function(d) {
+    d.Longitude = +d.Longitude;
+    d.Latitude = +d.Latitude;
+  }); */
+    
+    
+});
+
+
+
 (function(){
 	var uStatePaths=[
 
