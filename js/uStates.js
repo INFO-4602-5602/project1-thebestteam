28c82a7cac7b5a972374b9d3c5d553812e515168
tamo1123 <taylormoede@gmail.com>
2017-04-11 16:16:08 -0600

    Adding new map and cleaning js

--- js/uStates.js
+++ js/uStates.js
@@ -27,11 +27,28 @@ d3.csv("data/ZayoHackathonData_Buildings.csv", function(csvDataBuilding) {
     })
     console.log("Long: ", LongData);
     console.log("Lati: ", LatiData);
-    
-   /* data.forEach(function(d) {
-    d.Longitude = +d.Longitude;
-    d.Latitude = +d.Latitude;
-  }); */
-    
-    
 });
+
+var map = L.map('map').setView([39.74739, -105], 13);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.light'
+	}).addTo(map);
+
+	var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+    L.geoJSON(geojsonFeature).addTo(map);
