7496bfaadbdc34b9a1119b0353170e27034ed025
tamo1123 <taylormoede@gmail.com>
2017-04-11 16:05:32 -0600

    Clean slate for new viz, ready to start with leaflet

--- js/uStates.js
+++ js/uStates.js
@@ -20,8 +20,10 @@ var LatiData = [];
 
 d3.csv("data/ZayoHackathonData_Buildings.csv", function(csvDataBuilding) {
     csvDataBuilding.map(function(d){
+        if (d.State == "CO"){
         LongData.push(+d.Longitude);
         LatiData.push(+d.Latitude);
+        }
     })
     console.log("Long: ", LongData);
     console.log("Lati: ", LatiData);
@@ -33,36 +35,3 @@ d3.csv("data/ZayoHackathonData_Buildings.csv", function(csvDataBuilding) {
     
     
 });
-
-
-
-(function(){
-	var uStatePaths=[
-
-{id:"CO",n:"Colorado",d:"M380.03242,320.96457L384.93566,234.63961L271.5471,221.99565L259.33328,309.93481L380.03242,320.96457Z"},
-
-
-
-	];
-	var uStates={};
-		
-	uStates.draw = function(id, data, toolTip){		
-		function mouseOver(d){
-			d3.select("#tooltip").transition().duration(200).style("opacity", .9);      
-			
-			d3.select("#tooltip").html(toolTip(d.n, data[d.id]))  
-				.style("left", (d3.event.pageX) + "px")     
-				.style("top", (d3.event.pageY - 28) + "px");
-		}
-		
-		function mouseOut(){
-			d3.select("#tooltip").transition().duration(500).style("opacity", 0);      
-		}
-		
-		d3.select(id).selectAll(".state")
-			.data(uStatePaths).enter().append("path").attr("class","state").attr("d",function(d){ return d.d;})
-			.style("fill",function(d){ return data[d.id].color; })
-			.on("mouseover", mouseOver).on("mouseout", mouseOut);
-	}
-	this.uStates=uStates;
-})();
