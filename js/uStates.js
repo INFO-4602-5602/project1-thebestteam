857c25e55120d0ad61adc147c772c6c44894975d
danvn <dan.nguyen7@yahoo.com>
2017-04-12 09:31:51 -0600

    Plots first 25 Colorado Buildings, trying to get it to plot :o: because it looks cooler

--- js/uStates.js
+++ js/uStates.js
@@ -14,15 +14,16 @@ function checkDataset(dataset) {
     else
         $("#dataCheck").append("<p>Data loaded incorrectly. Try using the debugger to help you find the bug!</p>");
 }
+
+// Hold building position in arrays
 var LongData = [];
 var LatiData = [];
-
 
 d3.csv("data/ZayoHackathonData_Buildings.csv", function(error, data) {
     data.map(function(d){
         if (d.State == "CO"){
-        LongData.push(+d.Longitude);
-        LatiData.push(+d.Latitude);
+            LongData.push(+d.Longitude);
+            LatiData.push(+d.Latitude);
         }
     })
     console.log("Long: ", LongData);
@@ -31,30 +32,68 @@ d3.csv("data/ZayoHackathonData_Buildings.csv", function(error, data) {
 ////////////////////////////
 
 
-var map = L.map('map').setView([39.74739, -105], 8);
-
+var map = L.map('map').setView([39.75621, -104.99404], 14);
+    // Set up map box
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
             '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
             'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.light'
+        id: 'mapbox.streets'
     }).addTo(map);
+    
+    
+ 
+    for (i = 0; i < 25; i++){
+        var building = {
+            "Latitude": data[i].Latitude,
+            "Longitude": data[i].Longitude
+        }   
+        console.log(building)
+        
+//        L.circle([building.Longitude, building.Latitude], 300, {
+//            color: 'red',
+//            fillColor: 'red',
+//            fillOpacity: 0.5
+//        }).addTo(map).bindPopup("I am a circle.");
+        
+        var Coors = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [LongData[i], LatiData[i]]
+        }
+        };
+        L.geoJSON(Coors).addTo(map);
 
-    var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
+
     }
-};
-    L.geoJSON(geojsonFeature).addTo(map);
 
+    
+    
+    L.circle([39.75621, -104.99404], 500, {
+		color: 'red',
+		fillColor: 'red',
+		fillOpacity: 0.5
+	}).addTo(map).bindPopup("I am a circle.");
+
+    // Coors Field Single Point
+    var Coors = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-104.99404, 39.75621]
+        }
+    };
+    L.geoJSON(Coors).addTo(map);
+
+    var circle = L.circle([-104.99404, 39.75622], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(map);
+    
     var zayo = {
     "type": "Feature",
     "properties": {
@@ -67,17 +106,12 @@ var map = L.map('map').setView([39.74739, -105], 8);
         "coordinates": [LongData[2], LatiData[2]]
     }
 };
-    L.geoJSON(zayo).addTo(map);    
+    L.geoJSON(zayo).addTo(map);   
+    
 
+  
 
-    var hayo = {
-    "type": "Feature",
-    "geometry": {
-        "type": "Point",
-        "coordinates": [data[4].Longitude, data[4].Latitude]
-    }
-};
-    L.geoJSON(hayo).addTo(map);        
+    
 
 
 });
