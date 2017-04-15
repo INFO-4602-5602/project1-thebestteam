7ac69d62395d01ec453f189e5fedf64f56bc1875
Elias Ortiz <elias.ortiz@colorado.edu>
2017-04-15 15:10:17 -0600

    added the second graph of how much zayo is currently making

--- js/nonspatial2.js
+++ js/nonspatial2.js
@@ -28,7 +28,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
 	//CO
 	//non-empty NPV value
 	ethernetCPQData = _.filter(ethernetCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Ethernet';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Ethernet';
 	});
 
 	ethernetCPQData = _.map( ethernetCPQData, function(d){
@@ -44,7 +44,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //CO
     //non-empty NPV value
     waveMetroCPQData = _.filter(waveMetroCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Wavelengths - Metro';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Wavelengths - Metro';
 	});
 
 	waveMetroCPQData = _.map( waveMetroCPQData, function(d){
@@ -60,7 +60,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //CO
     //non-empty NPV value
     IPCPQData = _.filter(IPCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'IP Services';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'IP Services';
 	});
 
 	IPCPQData = _.map( IPCPQData, function(d){
@@ -76,7 +76,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //CO
     //non-empty NPV value
     zColoCPQData = _.filter(zColoCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'zColo';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'zColo';
 	});
 
 	zColoCPQData = _.map( zColoCPQData, function(d){
@@ -92,7 +92,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //CO
     //non-empty NPV value
     waveLongCPQData = _.filter(waveLongCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Wavelengths - Long Haul';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Wavelengths - Long Haul';
 	});
 
 	waveLongCPQData = _.map( waveLongCPQData, function(d){
@@ -108,7 +108,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //CO
     //non-empty NPV value
     darkCPQData = _.filter(darkCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Dark Fiber - Metro';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Dark Fiber - Metro';
 	});
 
 	darkCPQData = _.map( darkCPQData, function(d){
@@ -124,7 +124,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //CO
     //non-empty NPV value
     SONETCPQData = _.filter(SONETCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'SONET';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'SONET';
 	});
 
 	SONETCPQData = _.map( SONETCPQData, function(d){
@@ -140,7 +140,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //CO
     //non-empty NPV value
     videoCPQData = _.filter(videoCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Live Video';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Live Video';
 	});
 
 	videoCPQData = _.map( videoCPQData, function(d){
@@ -156,7 +156,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //CO
     //non-empty NPV value
     cloudCPQData = _.filter(cloudCPQData, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Cloud';
+		return d['X36 NPV List'] !== ' $-   ' && d['Product Group'] === 'Cloud';
 	});
 
 	cloudCPQData = _.map( cloudCPQData, function(d){
@@ -170,7 +170,7 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
     //generic to build the graph
 
     CPQdata = _.filter(CPQdata, function(d){
-		return d['State'] === 'CO' && d['X36 NPV List'] !== ' $-   ';
+		return d['X36 NPV List'] !== ' $-   ';
 	});
 
 	CPQdata = _.map( CPQdata, function(d){
@@ -242,5 +242,84 @@ d3.csv('data/ZayoHackathonData_CPQs.csv', function(data) {
 		         .attr( 'width', 75)
 		         .attr( 'y', function(d){ return yScale(d['X36 NPV List']) + yOffset + 20; } )
 		         .attr( 'x', function( d ){ return xScale(d['Product Group']) + xOffset + 20; } )
-		         .style('fill', 'green');
+		         .style('fill', 'blue');
+});
+
+d3.csv('data/ZayoHackathonData_Services.csv', function(data){
+	var servicesData = data;
+
+	servicesData = _.filter(servicesData, function(d){
+		return ((d[' Total MRR '] !== ' $-   ') && (d['Product Group'] === 'Ethernet' || d['Product Group'] === 'Wavelengths - Metro' || d['Product Group'] === 'IP Services' || d['Product Group'] === 'Wavelengths - Long Haul' || d['Product Group'] === 'zColo' || d['Product Group'] === 'Dark Fiber - Metro' || d['Product Group'] === 'SONET' || d['Product Group'] === 'Cloud' || d['Product Group'] === 'Live Video'));
+	});
+
+	servicesData = _.map( servicesData, function(d){
+      return {
+        'Account ID': d['Account ID'],
+        'Product_Group': d['Product Group'],
+        ' Total MRR ': Number(d[' Total MRR '].replace(/[^0-9\.]+/g,""))
+      }
+    });
+
+    servicesData = _.mapKeys( servicesData, function( d ){
+      return d['Product_Group'];
+    });
+
+    productMRRs = _(servicesData)
+    	.groupBy('Product_Group')
+    	.map((val, key) => ({
+    		'Product_Group': key,
+    		' Total MRR ': _.sumBy(val, ' Total MRR '),
+    	})).value();
+
+    var svg = d3.select( '#nonspatial2_2' ).append('svg:svg')
+    			.attr('width', width)
+    			.attr('height', height);
+
+    productArray2 = _.map(productMRRs, function(d){
+    	return d['Product_Group']
+    });
+
+    var xScale = d3.scale.ordinal()
+    					 .domain(productArray2)
+    					 .rangeRoundBands([0, width - margin - 50]);
+
+    var yScale = d3.scale.linear()
+				    	 .domain([0, d3.max(productMRRs, function(d){
+				    	 	return parseFloat(d[' Total MRR ']);
+				    	 })+1])
+				    	 // .domain([d3.max(productNPVs, function(d){
+				    	 // 	return parseFloat(d['X36 NPV List']);
+				    	 // })+1,0])
+				    	 .range([height - yOffset - margin, margin]);
+
+    var xAxis = d3.svg.axis()
+			    	  .scale(xScale)
+			    	  .orient('bottom');
+    var xAxisG = svg.append('g')
+			    	.attr('class','axis')
+			    	.attr('transform','translate('+ xOffset +', '+ ( height - yOffset) +')')
+			    	.call(xAxis);
+
+    var yAxis = d3.svg.axis()
+			    	  .scale(yScale)
+			    	  .orient('left')
+			    	  .ticks(10);
+    var yAxisG = svg.append('g')
+			    	.attr('class', 'axis')
+			    	.attr('transform', 'translate(' + (xOffset) + ',0)')
+			    	.call(yAxis);
+
+	var title = d3.select( '#title2' ).append( 'text' )
+                  .style( 'font-size', '40px' )
+                  .text( 'Current Revenue Per Product Group' );
+
+    var bar = svg.selectAll( 'rect' )
+                 .data( productMRRs )
+                 .enter()
+                 .append( 'rect' )
+       			 .attr( 'height', function( d ){ return height - yScale(d[' Total MRR ']) - xOffset; } )//d['X36 NPV List']) - yOffset
+		         .attr( 'width', 75)
+		         .attr( 'y', function(d){ return yScale(d[' Total MRR ']) + yOffset + 20; } )
+		         .attr( 'x', function( d ){ return xScale(d['Product_Group']) + xOffset + 20; } )
+		         .style('fill', 'red');
 });
